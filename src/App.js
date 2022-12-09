7714daa07da045993a7c5986553d59ac97d15095
Akash Ingole <akash214ingole@gmail.com>
2022-12-09 06:52:04 +0000

    Made an update in app.js file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <h3>This is another heading. Just for testing</h3>
+      <h3>This is another heading. Just for testing</h3>
     </div>
   );
 }
