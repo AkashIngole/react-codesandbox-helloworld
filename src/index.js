fb5955e8054ec09a193c668c5999452f18349339
Akash Ingole <akash214ingole@gmail.com>
2022-12-22 03:31:40 +0000

    Added React.Fragment and variable expresssions in HTML tags using {} braces.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = 'Arjun';
+
+
+ReactDOM.render(
+  <>
+    <h1>Hello World!</h1>
+    <p> This is a pararaph inside render </p>
+    <p> This is another tag in div element of render method</p>
+    <h1>My name is {name}</h1>
+    <p>My lucky number is {5 + 4} </p>
+    <p>The random number is {Math.random()}</p>
+  </>,
+  document.getElementById("root")
+);
+
+// Traditional way using javascript
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hey World!";
+// document.getElementById("root").appendChild(h1);
